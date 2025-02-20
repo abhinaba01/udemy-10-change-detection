852610c902b3acb9404461a596d926094e085574
abhinaba01 <abhinaba.mandal@capgemini.com>
2025-02-20 14:27:52 +0530

    Manual Change Detection

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,13 +1,28 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, input } from '@angular/core';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
+  changeDetection:ChangeDetectionStrategy.OnPush,
 })
 export class MessagesListComponent {
-  messages = input.required<string[]>();
+ private messageService = inject(MessagesService)
+ private cdRef = inject(ChangeDetectorRef);
+
+ messages: string[]=[];
+
+ ngOnInit(){
+  this.messageService.message$.subscribe((messages)=>{
+    this.messages=messages
+    this.cdRef.markForCheck();
+  });
+ }
+ 
+
+ 
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
